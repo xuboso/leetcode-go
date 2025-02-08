baafbdb478535a364171f61376d2daff650ae1ee
bude <xubobox@gmail.com>
2025-02-08 19:14:26 +0800

    旋转数组

---
+++ rotate_array.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+/**
+ * 旋转数组
+ * https://leetcode.cn/problems/rotate-array/
+ */
+func main() {
+
+	nums := []int{1, 2, 3, 4, 5, 6, 7}
+	k := 3
+	rotate(nums, k)
+	fmt.Println(nums)
+}
+
+func rotate(nums []int, k int) {
+	newNums := make([]int, len(nums))
+	for i, v := range nums {
+		newNums[(i+k)%len(nums)] = v
+	}
+	copy(nums, newNums)
+}
