8f6812e97cec43add4819544bcb11ab224dadcb9
bude <xubobox@gmail.com>
2025-02-10 21:53:43 +0800

    最长公共前缀

---
+++ longest_common_prefix.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+/**
+ * 最长公共前缀
+ * https://leetcode.cn/problems/longest-common-prefix/
+ */
+func main() {
+
+	strs := []string{"flower", "flow", "flight"}
+
+	fmt.Println(longestCommonPrefix(strs))
+}
+
+func longestCommonPrefix(strs []string) string {
+	s0 := strs[0]
+	for j := range s0 {
+		for _, s := range strs {
+			if j == len(s) || s[j] != s0[j] {
+				return s0[:j]
+			}
+		}
+	}
+	return s0
+}
