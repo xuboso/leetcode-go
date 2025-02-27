c669a0982c00da03f909efee7fa28c65cc1b8d59
bude <xubobox@gmail.com>
2025-02-27 20:47:22 +0800

    设计模式: 桥接模式

---
+++ 设计模式/bridge.go
@@ -0,0 +1,79 @@
+/**
+ * 桥接模式是一种结构型设计模式， 可将一个大类或一系列紧密相关的类拆分为抽象和实现两个独立的层次结构， 从而能在开发时分别使用。
+ * https://refactoringguru.cn/design-patterns/bridge
+ */
+package main
+
+import "fmt"
+
+type Computer interface {
+	Print()
+	SetPrinter(Printer)
+}
+
+type Mac struct {
+	printer Printer
+}
+
+func (m *Mac) Print() {
+	fmt.Println("Print request for mac")
+	m.printer.PrintFile()
+}
+
+func (m *Mac) SetPrinter(p Printer) {
+	m.printer = p
+}
+
+type Windows struct {
+	printer Printer
+}
+
+func (w *Windows) Print() {
+	fmt.Println("Print request for windows")
+	w.printer.PrintFile()
+}
+
+func (w *Windows) SetPrinter(p Printer) {
+	w.printer = p
+}
+
+type Printer interface {
+	PrintFile()
+}
+
+type Epson struct{}
+
+func (p *Epson) PrintFile() {
+	fmt.Println("Printing by a EPSON Printer")
+}
+
+type Hp struct{}
+
+func (p *Hp) PrintFile() {
+	fmt.Println("Printing by a HP Printer")
+}
+
+func main() {
+
+	hpPrinter := &Hp{}
+	epsonPrinter := &Epson{}
+
+	macComputer := &Mac{}
+	macComputer.SetPrinter(hpPrinter)
+	macComputer.Print()
+	fmt.Println()
+
+	macComputer.SetPrinter(epsonPrinter)
+	macComputer.Print()
+	fmt.Println()
+
+	winComputer := &Windows{}
+
+	winComputer.SetPrinter(hpPrinter)
+	winComputer.Print()
+	fmt.Println()
+
+	winComputer.SetPrinter(epsonPrinter)
+	winComputer.Print()
+	fmt.Println()
+}
