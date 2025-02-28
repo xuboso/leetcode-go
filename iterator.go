bbe0e11ba6d9862155277760c9e4a9c2bfc99100
bude <xubobox@gmail.com>
2025-02-28 21:05:11 +0800

    设计模式: 迭代器模式

---
+++ iterator.go
@@ -0,0 +1,74 @@
+/**
+ * 迭代器模式是一种行为设计模式， 让你能在不暴露集合底层表现形式 （列表、 栈和树等） 的情况下遍历集合中所有的元素。
+ * https://refactoringguru.cn/design-patterns/iterator
+ */
+package main
+
+import "fmt"
+
+type Collection interface {
+	createIterator() Iterator
+}
+
+type UserCollection struct {
+	users []*User
+}
+
+func (u *UserCollection) createIterator() Iterator {
+	return &UserIterator{
+		users: u.users,
+	}
+}
+
+type Iterator interface {
+	hasNext() bool
+	getNext() *User
+}
+
+type UserIterator struct {
+	index int
+	users []*User
+}
+
+func (u *UserIterator) hasNext() bool {
+	if u.index < len(u.users) {
+		return true
+	}
+	return false
+}
+
+func (u *UserIterator) getNext() *User {
+	if u.hasNext() {
+		user := u.users[u.index]
+		u.index++
+		return user
+	}
+	return nil
+}
+
+type User struct {
+	name string
+	age  int
+}
+
+func main() {
+
+	user1 := &User{
+		name: "a",
+		age:  30,
+	}
+	user2 := &User{
+		name: "b",
+		age:  20,
+	}
+
+	UserCollection := &UserCollection{
+		users: []*User{user1, user2},
+	}
+
+	iterator := UserCollection.createIterator()
+	for iterator.hasNext() {
+		user := iterator.getNext()
+		fmt.Printf("User is %+v\n", user)
+	}
+}
