7b1159818927838c5ebdc1f3edafa87cba877b9c
bude <xubobox@gmail.com>
2025-02-28 15:43:56 +0800

    设计模式: 代理模式

---
+++ 设计模式/proxy.go
@@ -0,0 +1,80 @@
+/**
+ * 代理模式是一种结构型设计模式， 让你能够提供对象的替代品或其占位符。 代理控制着对于原对象的访问， 并允许在将请求提交给对象前后进行一些处理。
+ * https://refactoringguru.cn/design-patterns/proxy
+ */
+package main
+
+import "fmt"
+
+type server interface {
+	handleRequest(string, string) (int, string)
+}
+
+type Nginx struct {
+	application       *Application
+	maxAllowedRequest int
+	rateLimiter       map[string]int
+}
+
+func newNginxServer() *Nginx {
+	return &Nginx{
+		application:       &Application{},
+		maxAllowedRequest: 2,
+		rateLimiter:       make(map[string]int),
+	}
+}
+
+func (n *Nginx) handleRequest(url, method string) (int, string) {
+	allowed := n.checkRateLimiting(url)
+	if !allowed {
+		return 403, "Not Allowed"
+	}
+	return n.application.handleRequest(url, method)
+}
+
+func (n *Nginx) checkRateLimiting(url string) bool {
+	if n.rateLimiter[url] == 0 {
+		n.rateLimiter[url] = 1
+	}
+	if n.rateLimiter[url] > n.maxAllowedRequest {
+		return false
+	}
+	n.rateLimiter[url] = n.rateLimiter[url] + 1
+	return true
+}
+
+type Application struct{}
+
+func (a *Application) handleRequest(url, method string) (int, string) {
+	if url == "/app/status" && method == "GET" {
+		return 200, "Ok"
+	}
+
+	if url == "/create/user" && method == "POST" {
+		return 201, "User Created"
+	}
+
+	return 404, "Not Ok"
+}
+
+func main() {
+
+	nginxServer := newNginxServer()
+	appStatusURL := "/app/status"
+	createUserURL := "/create/user"
+
+	httpCode, body := nginxServer.handleRequest(appStatusURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+	httpCode, body = nginxServer.handleRequest(appStatusURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+	httpCode, body = nginxServer.handleRequest(appStatusURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+	httpCode, body = nginxServer.handleRequest(createUserURL, "POST")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+	httpCode, body = nginxServer.handleRequest(createUserURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+}
