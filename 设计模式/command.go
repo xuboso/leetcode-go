e21af8628df6853112c38dfd2ad559c124b12c91
bude <xubobox@gmail.com>
2025-03-01 14:33:09 +0800

    设计模式: 命令模式

---
+++ 设计模式/command.go
@@ -0,0 +1,76 @@
+/**
+ * 命令模式是一种行为设计模式， 它可将请求转换为一个包含与请求相关的所有信息的独立对象。 该转换让你能根据不同的请求将方法参数化、 延迟请求执行或将其放入队列中， 且能实现可撤销操作。
+ * https://refactoringguru.cn/design-patterns/command
+ */
+package main
+
+import "fmt"
+
+type Button struct {
+	command Command
+}
+
+func (b *Button) press() {
+	b.command.execute()
+}
+
+type Command interface {
+	execute()
+}
+
+type OnCommand struct {
+	device Device
+}
+
+func (c *OnCommand) execute() {
+	c.device.on()
+}
+
+type OffCommand struct {
+	device Device
+}
+
+func (c *OffCommand) execute() {
+	c.device.off()
+}
+
+type Device interface {
+	on()
+	off()
+}
+
+type Tv struct {
+	isRunning bool
+}
+
+func (t *Tv) on() {
+	t.isRunning = true
+	fmt.Println("Turning tv on")
+}
+
+func (t *Tv) off() {
+	fmt.Println("Turning tv off")
+}
+
+func main() {
+
+	tv := &Tv{}
+
+	onCommand := &OnCommand{
+		device: tv,
+	}
+
+	offCommand := &OffCommand{
+		device: tv,
+	}
+
+	onButton := &Button{
+		command: onCommand,
+	}
+	onButton.press()
+
+	offButton := &Button{
+		command: offCommand,
+	}
+	offButton.press()
+}
